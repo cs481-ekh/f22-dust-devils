c7dde9d298b9b727da8c755bf7737d2a28e6e8dd
lukaskarlsson290 <73920347+lukaskarlsson290@users.noreply.github.com>
2022-10-08 16:08:27 -0600

    Test for TTS + general error fixes (#49)

--- muldoon/__init__.py
+++ muldoon/__init__.py
@@ -5,6 +5,6 @@ __version__ = '1.3.5'
 __author__ = 'Brian Jackson <bjackson@boisestate.edu>'
 __all__ = ['met_timeseries', 'utils', 'read_data']
 
-from .met_timeseries import *
-from .utils import *
-from .read_data import *
+from muldoon.met_timeseries import *
+from muldoon.utils import *
+from muldoon.read_data import *
