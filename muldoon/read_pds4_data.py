012bd041d6fa77e9bf35aec066381fc4cc32de2b
Senami Hodonu <84221854+senamihodonu@users.noreply.github.com>
2022-10-11 11:44:37 -0600

    75 pds4 test file (#76)
    * test pds4 file
    
    * pds4 test file
    
    * window os special allowance - ssl
    
    * window os special allowance - ssl
    
    Co-authored-by: root <root@LAPTOP-IQ8G0K6B.localdomain>

--- muldoon/read_pds4_data.py
+++ muldoon/read_pds4_data.py
@@ -4,9 +4,18 @@ A collections of routines to read in data from various missions (PDS4 format)
 from urllib.request import urlretrieve
 from pds4_tools import pds4_read
 import matplotlib.pyplot as plt
+import platform
 import ssl
 
 def readInputFile(filename:str):
-    ssl._create_default_https_context = ssl._create_unverified_contextx
+    """
+    Read in PDS4 file
+    Args:
+        filename (str): path to PDS4 xml/link
+    Returns:
+        Data information contained in PDS4 label
+    """
+    if(platform.system() == 'Windows'):
+        ssl._create_default_https_context = ssl._create_unverified_contextx
     data = pds4_read(filename)
     return data.info()
