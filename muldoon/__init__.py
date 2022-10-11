cdda66a9c4a9406f5e2809f37e18a5ca45583e8c
Samantha Farmer <70185203+samfarmer1@users.noreply.github.com>
2022-10-11 12:21:07 -0600

    21 test 1 pressure time series (#56)
    * Completes #25
    
    * Updates dictionary
    
    * Implements test_init()
    
    * fixed pytest #43
    
    * deleted obsolete test

--- muldoon/__init__.py
+++ muldoon/__init__.py
@@ -5,6 +5,7 @@ __version__ = '1.3.5'
 __author__ = 'Brian Jackson <bjackson@boisestate.edu>'
 __all__ = ['met_timeseries', 'utils', 'read_data']
 
-from muldoon.met_timeseries import *
-from muldoon.utils import *
-from muldoon.read_data import *
+from muldoon import met_timeseries
+from muldoon import utils
+from muldoon import read_data
+
