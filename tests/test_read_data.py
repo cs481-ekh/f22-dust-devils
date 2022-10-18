95870fb85f3b5cdd27a59d66e106952d9dcd9ec3
root <root@LAPTOP-IQ8G0K6B.localdomain>
2022-10-17 23:13:59 -0600

    read_data mod

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -3,25 +3,29 @@ from numpy.random import normal
 from muldoon import met_timeseries as met
 from muldoon import utils 
 from muldoon.read_data import *
+from pds4_tools import pds4_read
+import pandas as pd
+
+from muldoon.read_data import file_verification
 
 def test_file_verification_pds4_file_exist():
-    data =  file_verification('https://pdssbn.astro.umd.edu/holdings/pds4-gbo-kpno:hyakutake_spectra-v1.0/data/offset_0_arcsec.xml')
-    expect_file_status = 1
-    assert(data == expect_file_status)
+    filename = 'https://pdssbn.astro.umd.edu/holdings/pds4-gbo-kpno:hyakutake_spectra-v1.0/data/offset_0_arcsec.xml'
+    file_status, _ =  file_verification(filename)
+    assert(file_status == 1)
 
 def test_file_verification_pds4_file_does_not_exist():
-    data =  file_verification('https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_calibrated_env/sol_0000_0089/sol_0001/WE__0001___________CAL_ATS_________________P1.xml')
+    filename = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_calibrated_env/sol_0000_0089/sol_0001/WE__0001___________CAL_ATS_________________P1.xml'
+    file_status, _ =  file_verification(filename)
     # wrong link
-    expect_file_status = 0
-    assert(data == expect_file_status)
+    assert(file_status == 0)
 
 def test_file_verification_csv_file_exist():
-    data =  file_verification('./tests/WE__0001___________DER_PS__________________P02.csv')
-    expect_file_status = 1
-    assert(data == expect_file_status)
+    filename = './tests/WE__0001___________DER_PS__________________P02.csv'
+    file_status, _ =  file_verification(filename)
+    assert(file_status == 1)
 
 def test_file_verification_csv_file_does_not_exist():
-    data =  file_verification('./tests/WE__0001___________DER_PS__________________P0.csv')
+    filename = './tests/WE__0001___________DER_PS__________________P0.csv'
+    file_status, _ =  file_verification(filename)
     # wrong csv file
-    expect_file_status = 0
-    assert(data == expect_file_status)
+    assert(file_status == 0)
