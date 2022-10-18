7cbd5d7ca73648518193b934955c3b21818fff34
Senami Hodonu <84221854+senamihodonu@users.noreply.github.com>
2022-10-17 22:34:42 -0600

    file verification function (#94)
    * file verification function
    
    * test csv file

--- tests/test_pds4.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from numpy.random import normal
-from muldoon import met_timeseries as met
-from muldoon import utils 
-from muldoon.read_data import *
-from muldoon.read_pds4_data import *
-
-def test_read_input_file():
-    data =  readInputFile('https://pdssbn.astro.umd.edu/holdings/pds4-gbo-kpno:hyakutake_spectra-v1.0/data/offset_0_arcsec.xml')
