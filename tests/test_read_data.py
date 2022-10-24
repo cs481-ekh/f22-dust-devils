b0b62433f301e7a345d1f9cf93314932bbb2d943
Senami Hodonu <84221854+senamihodonu@users.noreply.github.com>
2022-10-24 16:51:35 -0600

    87-modify-read_perseverance_ats_data-to-read-pds4-label (#107)
    Co-authored-by: root <root@LAPTOP-IQ8G0K6B.localdomain>

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -8,10 +8,14 @@ from muldoon.read_data import *
 from pds4_tools import pds4_read
 import pandas as pd
 
-pressure_pds4_file = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0000_0089/sol_0001/WE__0001___________DER_PS__________________P02.xml'
+pressure_pds4_file = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0000_0089/sol_0001/WE__0001___________DER_PS__________________P02.xml' #remote
+# pressure_pds4_file = './tests/WE__0001___________DER_PS__________________P02.xml' #local
 pressure_csv_file = './tests/WE__0001___________DER_PS__________________P02.csv'
-wind_pds4_file = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0180_0299/sol_0190/WE__0190___________DER_WS__________________P02.xml'
+wind_pds4_file = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0180_0299/sol_0190/WE__0190___________DER_WS__________________P02.xml' #remote
+# wind_pds4_file = 'WE__0190___________DER_WS__________________P02.xml' #local
 wind_csv_file = './tests/WE__0190___________DER_WS__________________P02.csv'
+ats_csv_file = './tests/WE__0010___________CAL_ATS_________________P01.csv'
+ats_pds4_file = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_calibrated_env/sol_0000_0089/sol_0010/WE__0010___________CAL_ATS_________________P01.xml'
 
 def test_file_verification_pds4_file_exist():
     _, file_status =  read_data(pressure_pds4_file) 
@@ -93,7 +97,6 @@ def test_read_Perseverance_WS_data_csv_hwsu():
 def test_read_Perseverance_WS_data_pds4_vws():
     _,result = read_Perseverance_WS_data(wind_pds4_file, sol=None, time_field='lmst', wind_field='vws')
     expected = [0.0]
-    print(result)
     assert(result[0] == expected[0])
 
 def test_read_Perseverance_WS_data_csv_vws():
@@ -103,7 +106,6 @@ def test_read_Perseverance_WS_data_csv_vws():
 def test_read_Perseverance_WS_data_pds4_vwsu():
     _,result = read_Perseverance_WS_data(wind_pds4_file, sol=None, time_field='lmst', wind_field='vwsu')
     expected = [0.0]
-    print(result)
     assert(result[0] == expected[0])
 
 def test_read_Perseverance_WS_data_csv_vwsu():
@@ -113,7 +115,6 @@ def test_read_Perseverance_WS_data_csv_vwsu():
 def test_read_Perseverance_WS_data_pds4_wd():
     _,result = read_Perseverance_WS_data(wind_pds4_file, sol=None, time_field='lmst', wind_field='wd')
     expected = [269.33]
-    print(result)
     assert(result[0] == expected[0])
 
 def test_read_Perseverance_WS_data_csv_wd():
@@ -148,3 +149,30 @@ def test_read_Perseverance_WS_data_csv_rs():
     _,result = read_Perseverance_WS_data(wind_csv_file, sol=None, time_field='lmst', wind_field='rs')
     expected = [1]
     assert(result[0] == expected[0])
+
+def test_read_Perseverance_ATS_data_ATS1():
+    _,result = read_Perseverance_ATS_data(ats_csv_file,1,'LTST',None)
+    expected = 240.67
+    assert(result[0]== expected)
+
+def test_read_Perseverance_ATS_data_ATS2():
+    _,result = read_Perseverance_ATS_data(ats_csv_file,2,'LTST',None)
+    expected = 257.01
+    assert(result[0]== expected)
+
+def test_read_Perseverance_ATS_data_ATS3():
+    _,result = read_Perseverance_ATS_data(ats_csv_file,3,'LTST',None)
+    expected = 240.06
+    assert(result[0]== expected)
+
+def test_read_Perseverance_ATS_data_ATS4():
+    _,result = read_Perseverance_ATS_data(ats_csv_file,4,'LTST',None)
+    expected = 243.92
+    assert(result[0]== expected)
+
+def test_read_Perseverance_ATS_data_ATS5():
+    _,result = read_Perseverance_ATS_data(ats_csv_file,5,'LTST',None)
+    expected = 250.99
+    assert(result[0]== expected)
+
+
