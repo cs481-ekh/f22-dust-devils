69550dd278df5d2dc266dcb08f27c0b7dc4d6b28
Senami Hodonu <84221854+senamihodonu@users.noreply.github.com>
2022-11-24 03:10:40 -0700

    Update read_data.py

--- muldoon/read_data.py
+++ muldoon/read_data.py
@@ -524,7 +524,7 @@ def plot_Perseverance_Pressure_Data(filename, sol=None, time_field = 'LTST', sta
     plt.show()
 
 
-filename = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0000_0089/sol_0001/WE__0001___________DER_PS__________________P02.xml' #remote
+# filename = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0000_0089/sol_0001/WE__0001___________DER_PS__________________P02.xml' #remote
 # pressure_pds4_file = './tests/WE__0001___________DER_PS__________________P02.xml' #local
 # filename = './tests/WE__0001___________DER_PS__________________P02.csv'
 # filename = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0180_0299/sol_0190/WE__0190___________DER_WS__________________P02.xml' #remote
@@ -535,10 +535,10 @@ filename = 'https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_med
 
 
 
-time = make_seconds_since_midnight(filename, time_field='LMST', sol=None)
+# time = make_seconds_since_midnight(filename, time_field='LMST', sol=None)
 
 # plot_Perseverance_ATS_data(filename, which_ATS=2, save_file = True)
 
-print(time)
+# print(time)
 # print(time, temperature)
 
